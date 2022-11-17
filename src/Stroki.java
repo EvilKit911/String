850ea814ee44b7b8317d7be97e93031e750fed86
Nikita <19nikita98.3939@gmail.com>
2022-11-17 22:10:35 +0200

    задание 1

---
+++ src/Stroki.java
@@ -0,0 +1,14 @@
+public class Stroki {
+    public static void main(String[] args) {
+
+            // задание 1
+    String firstName = "Ivan";//для хранения имени
+    String middleName = "Ivanovich";//для хранения отчества
+    String lastName = "Ivanov"; //для хранения фамилии;
+
+    String fullName = lastName +" "+ firstName +" "+ middleName;
+
+        System.out.println("ФИО сотрудника " + fullName);
+
+    }
+}
