917ecdf358332874c3cb6da797c3d4fbea44f578
Nikita <19nikita98.3939@gmail.com>
2022-11-17 23:02:56 +0200

    задание 3

--- src/Stroki.java
+++ src/Stroki.java
@@ -2,9 +2,9 @@ public class Stroki {
     public static void main(String[] args) {
 
             // задание 1
-    String firstName = "Ivan";//для хранения имени
-    String middleName = "Ivanovich";//для хранения отчества
-    String lastName = "Ivanov"; //для хранения фамилии;
+    String firstName = "Семён";//для хранения имени
+    String middleName = "Семёнович";//для хранения отчества
+    String lastName = "Иванов"; //для хранения фамилии;
 
     String fullName = lastName +" "+ firstName +" "+ middleName;
 
@@ -15,5 +15,14 @@ public class Stroki {
             //задание 2
         String fullNameUp = fullName.toUpperCase(); // метод меняющий все символы в верхний регистр
         System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullNameUp);
+
+        System.out.println("");
+
+        //задание 3
+
+        fullName = fullName.replace("ё", "е");// метод replace(найти"условный знак или пробел",и заменить на "то что нам нужно")
+        System.out.println("Данные ФИО сотрудника " + fullName);
+
+
     }
 }
